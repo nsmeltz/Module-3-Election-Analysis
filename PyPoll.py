71df71a2b949df6d7653d2d7324577c0b9a3ee8d
nsmeltz <smeltznatalie@gmail.com>
2022-01-01 22:33:54 -0600

    Adding election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -2,7 +2,6 @@
 import csv
 import os
 
-## Import the data
 # Assign variable to path of import data file (election_results.csv)
 file_to_open = os.path.join("Module-3-Election-Analysis","Resources", "election_results.csv")
 # Assign variable to path of export data file (election_analysis.txt)
@@ -10,24 +9,67 @@ if not os.path.exists('Module-3-Election-Analysis/Analysis'): #if the folder doe
     os.makedirs('Module-3-Election-Analysis/Analysis')
 file_to_save = os.path.join("Module-3-Election-Analysis","Analysis", "election_analysis.txt")
 
-## Open the election results as a file object(election_data)
+# Initialize Variables
+total_votes = 0 
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+# Open the election results and read the file
 with open(file_to_open) as election_data :
     # Read file object with reader function
     file_reader = csv.reader(election_data)
-    # Print the header row.
+
+    # Print out all data starting at after headers from the import data CSV
     headers = next(file_reader)
-    print(headers)
-    # Print out all data without the headers from the import data file
     for row in file_reader :
-        print(row)
+        #Add up total vote count
+        total_votes += 1 
+
+        #Add candidate name to list if it is not already in the list
+        candidate_name = row[2]
+        if candidate_name not in candidate_options :
+            candidate_options.append(candidate_name)
+            # 3. Calculate total number of votes each candidate received
+            candidate_votes[candidate_name] = 0
+            # Add a vote to candidate's count
+        candidate_votes[candidate_name] += 1
+
+# Print Outputs to Terminal
+print(f" Total Number of Votes = {total_votes} ")
+print(f"Candidate Options: {candidate_options}")
+#print(f"Candidate Votes: {candidate_votes}")
+
+# 4. Calculated percentage of votes each candidate won & who won the election
+
+for candidate_name in candidate_votes :
+    votes = candidate_votes[candidate_name] #number of votes each candidate got
+    vote_percentage = float(votes) / float(total_votes) *100
+    
+    #print(f" {candidate_name} recieved {vote_percentage:.1f}% of the vote.")
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+     # If true then set winning_count = votes and winning_percent = vote_percentage.
+     winning_count = votes
+     winning_percentage = vote_percentage
+     #Set the winning_candidate equal to the candidate's name.
+     winning_candidate = candidate_name
 
 
+#Print winning candidate, vote count and percentage to terminal.
 
-# 1. Calculate total number of votes cast
-# 2. Complie a list of candidates who received votes
-# 3. Calculate total number of votes each candidate received
-# 4. Calculated percentage of votes each candidate won
-# 5. Determine the winner of the election based on popular vote
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+
 
 # Write output to text file
 with open(file_to_save, "w") as txt_file :
